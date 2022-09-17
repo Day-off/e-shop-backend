f85e7af5b20a1ab8abebae2dce5b033e552874ce
krsobo <krsobo@ttu.ee>
2022-09-17 22:02:06 +0300

    Initial commit

---
+++ src/main/java/com/example/iti0302backend/Iti0302BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.iti0302backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iti0302BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Iti0302BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iti0302backend/Iti0302BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iti0302backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iti0302BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

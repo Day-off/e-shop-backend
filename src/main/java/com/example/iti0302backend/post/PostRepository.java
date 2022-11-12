e225ea72979a5cecf4bb6ee8110b654fbb4d017a
krsobo <krsobo@ttu.ee>
2022-11-12 18:27:32 +0200

    delete duplicate repository

--- src/main/java/com/example/iti0302backend/post/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.iti0302backend.post;
-
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-import java.util.List;
-public interface PostRepository extends JpaRepositoryImplementation<Post, Integer> {
-    List<Post> findPostByHeadContainingIgnoreCase(String head);
-}

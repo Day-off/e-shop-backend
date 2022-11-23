aff670d39120e87637bbfec06156e862dbef34c7
jevolo <jevolo@ttu.ee>
2022-11-23 12:37:08 +0200

    Add password encryption.

--- src/main/java/com/example/iti0302backend/service/UserService.java
+++ src/main/java/com/example/iti0302backend/service/UserService.java
@@ -3,7 +3,9 @@ package com.example.iti0302backend.service;
 import com.example.iti0302backend.dto.UserDto;
 import com.example.iti0302backend.mapper.UserMapper;
 import com.example.iti0302backend.repository.UserRepository;
+import com.example.iti0302backend.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,13 +17,17 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
+    private final PasswordEncoder passwordEncoder;
+
     public List<UserDto> getUsers() {
         return userMapper.toDtoList(userRepository.findAll());
     }
 
     public void addUser(UserDto userDto) {
         try {
-            userRepository.save(userMapper.toUser(userDto));
+            User user = userMapper.toUser(userDto);
+            user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+            userRepository.save(user);
         } catch (Exception e) {
             e.printStackTrace();
         }

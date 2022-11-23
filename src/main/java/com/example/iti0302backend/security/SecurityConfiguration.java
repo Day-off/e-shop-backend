39488cb366e87955918909791059fe435478922b
jevolo <jevolo@ttu.ee>
2022-11-23 12:38:54 +0200

    Add password encryption.

--- src/main/java/com/example/iti0302backend/security/SecurityConfiguration.java
+++ src/main/java/com/example/iti0302backend/security/SecurityConfiguration.java
@@ -1,13 +1,9 @@
 package com.example.iti0302backend.security;
 
 import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @ConditionalOnDefaultWebSecurity

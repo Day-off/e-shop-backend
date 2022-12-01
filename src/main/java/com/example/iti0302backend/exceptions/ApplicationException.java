484d7d33fc5b1476e0eb1e017cbbba00d8adc90f
jevolo <jevolo@ttu.ee>
2022-12-01 20:15:53 +0200

    Improved security configuration and jwtFilter.

---
+++ src/main/java/com/example/iti0302backend/exceptions/ApplicationException.java
@@ -0,0 +1,8 @@
+package com.example.iti0302backend.exceptions;
+
+public class ApplicationException extends RuntimeException {
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+}

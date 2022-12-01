c5ae56a8c2298eb7bffd77cfb1112fa23a8fd5bc
damana <damana@ttu.ee>
2022-12-02 00:17:51 +0200

    style

--- src/main/test/com/example/iti0302backend/service/PostServiceTest.java
+++ src/main/test/com/example/iti0302backend/service/PostServiceTest.java
@@ -13,9 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;

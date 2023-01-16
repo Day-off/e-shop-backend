7f75c2cd75a64846fac6e33f81cd4e8a5b1ed543
krsobo <krsobo@ttu.ee>
2023-01-16 13:42:35 +0200

    fix delete orders

--- src/main/java/com/example/iti0302backend/repository/OrderRepository.java
+++ src/main/java/com/example/iti0302backend/repository/OrderRepository.java
@@ -2,6 +2,15 @@ package com.example.iti0302backend.repository;
 
 import com.example.iti0302backend.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into orders (customer, post) values (:userId, :postId)" ,nativeQuery=true)
+    void createOrder(int postId, int userId);
+
 }
